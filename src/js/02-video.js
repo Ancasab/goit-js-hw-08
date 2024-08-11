3ede9fb534ee00ee8d40d653e94b3e4ca602b1b1
Anca_Sab <asabadeanu@gmail.com>
2024-08-11 22:52:14 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,26 +6,6 @@ import throttle from 'lodash.throttle';
 // const iframe = document.querySelector('iframe');
 // const player = new Vimeo.Player(iframe);
 
-// const player = new Vimeo('vimeo-player');
-
-// const throttleSavePlaybackTime = throttle(function(time) {
-//   localStorage.setItem('videoplayer-current-time', time);
-// }, 1000);
-
-// player.on('timeupdate', function(data) {
-//   throttleSavePlaybackTime(data.seconds);
-// });
-
-// const currentTime = localStorage.getItem('videoplayer-current-time');
-// if (currentTime) {
-//   player.setCurrentTime(currentTime).then(function()   
-//  {
-//     player.play();
-//   }).catch(function(error) {
-//     console.error('Error:', error);
-//   });
-// }
-
 const player = new Vimeo('vimeo-player');
 
 const throttleSavePlaybackTime = throttle(savePlaybackTime, 1000);
@@ -43,4 +23,4 @@ if (currentTime) {
   player.setCurrentTime(currentTime).then(() => {
     player.play();
   });
-}
+};
